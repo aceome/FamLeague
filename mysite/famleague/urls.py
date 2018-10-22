2fa945ff55a2db2bcb261ca2160392e22bfea340
Aceome <angelina.bowmer@gmail.com>
2018-10-22 14:56:28 -0700

    capstone added pages and routes

--- mysite/famleague/urls.py
+++ mysite/famleague/urls.py
@@ -6,10 +6,11 @@ from . import views
 app_name = 'famleague'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index/', views.mylogin, name='mylogin'),
-    path('index/', views.logout_view, name='logout_view'),
+    path('login/', views.login_view, name='login_view'),
+    path('login_user/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
     path('register/', views.register, name='register'),
-    path('main/', views.main, name='main'),
+    path('register_user', views.register_user, name='register_user'),
     path('rules/', views.rules, name='rules'),
     path('leader/', views.leader, name='leader'),
     path('lineup/', views.lineup, name='lineup')
--- mysite/famleague/views.py
+++ mysite/famleague/views.py
@@ -6,41 +6,55 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-# @login_required
+@login_required
 def index(request):
     return render(request, 'famleague/index.html')
 
+def login_view(request):
+    return render(request, 'famleague/login.html')
 
-def mylogin(request):
+# receives the form submission
+def login_user(request):
     # retrieve the variables from the form submission
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return HttpResponseRedirect(reverse('famleague:main'))
+        return HttpResponseRedirect(reverse('famleague:index'))
     else:
-        return HttpResponse('That is not a valid Login.')
-        # return an 'invalid login' error message
+        return HttpResponseRedirect(reverse('famleague:login_view'))
 
-def logout_view(request):
+
+def logout_user(request):
     logout(request)
-    return HttpResponseRedirect(reverse('famleague:index'))
+    return HttpResponseRedirect(reverse('famleague:login_view'))
     # redirect to a success page.
 
 
+def register_user(request):
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    user = User.objects.create_user(username, email, password)
+    login(request, user)
+    return HttpResponseRedirect(reverse('famleague:index'))
+
+
 def register(request):
-    return render(request, 'famleague/register.html')
+    return render(request, 'famleague/.html')
 
 
-def main(request):
-    return render(request, 'famleague/main.html')
-
+@login_required
 def rules(request):
     return render(request, 'famleague/rules.html')
 
+
+@login_required
 def lineup(request):
     return render(request, 'famleague/lineup.html')
 
+
+@login_required
 def leader(request):
     return render(request, 'famleague/leader.html')
