7c650ef194bcddf9de1a23f32f07ef205530132e
Aceome <angelina.bowmer@gmail.com>
2018-10-22 10:46:30 -0700

    capstone added pages and routes

--- mysite/famleague/urls.py
+++ mysite/famleague/urls.py
@@ -6,7 +6,12 @@ from . import views
 app_name = 'famleague'
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('check_out/', views.check_out, name='check_out'),
-    # path('check_in/', views.check_in, name='check_in')
+    path('index/', views.mylogin, name='mylogin'),
+    path('index/', views.logout_view, name='logout_view'),
+    path('register/', views.register, name='register'),
+    path('main/', views.main, name='main'),
+    path('rules/', views.rules, name='rules'),
+    path('leader/', views.leader, name='leader'),
+    path('lineup/', views.lineup, name='lineup')
 
 ]
--- mysite/famleague/views.py
+++ mysite/famleague/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, reverse
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# @login_required
 def index(request):
     return render(request, 'famleague/index.html')
-
-
 
 
 def mylogin(request):
@@ -17,12 +18,29 @@ def mylogin(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return HttpResponseRedirect('famleague:main')
+        return HttpResponseRedirect(reverse('famleague:main'))
     else:
-        pass
+        return HttpResponse('That is not a valid Login.')
         # return an 'invalid login' error message
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('famleague:index'))
     # redirect to a success page.
+
+
+def register(request):
+    return render(request, 'famleague/register.html')
+
+
+def main(request):
+    return render(request, 'famleague/main.html')
+
+def rules(request):
+    return render(request, 'famleague/rules.html')
+
+def lineup(request):
+    return render(request, 'famleague/lineup.html')
+
+def leader(request):
+    return render(request, 'famleague/leader.html')
