d015ddb78240d2e575129acc3cf2006e94017a92
Aceome <angelina.bowmer@gmail.com>
2018-10-25 16:32:01 -0700

    capstone added model

--- mysite/famleague/admin.py
+++ mysite/famleague/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-# from .models import
+from .models import Team
 
 # Register your models here.
-# admin.site.register(User)
-# admin.site.register()
+admin.site.register(Team)
